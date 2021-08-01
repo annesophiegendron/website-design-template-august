d3e8c9370aa6895520103f84001f93f36a8885b8
annesophiegendron <annesophie.gendron@gmail.com>
2021-08-01 23:00:11 +0200

    color change added

--- my-app/src/components/Products.js
+++ my-app/src/components/Products.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styled from 'styled-components';
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 const Container = styled.section`
   display: flex;
@@ -8,8 +10,16 @@ const Container = styled.section`
 `
 
 const Left = styled.div`
-  background: red;
   flex-basis: 50%;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  position: relative;
+  min-height: 100vh;
+  width: 100%;
+  color: white;
+  flex-direction: column;
 `
 
 const Title = styled.h1`
@@ -17,7 +27,7 @@ const Title = styled.h1`
   position: sticky;
   top: 10px;
   margin-left: 20px;
-  color: white;
+  color: black;
   min-height: 100vh;
   display: block;
 `
@@ -33,13 +43,67 @@ const Image = styled.div`
   height: 100vh;
 `
 
+const Section = styled.section`
+width: 100%;
+
+height: 100vh;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+
 export const Products = () => {
+  const ref = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const sections = document.querySelectorAll(".color-section");
+    const initBgColor = document.body.dataset.initBgColor;
+
+    sections.forEach((section, i) => {
+      const prevColor = i === 0 ? initBgColor : sections[i - 1].dataset.bgColor;
+
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top center+=20%",
+        onEnter: () =>
+
+          gsap.to(".container", {
+            backgroundColor: section.dataset.bgColor,
+            duration: 0.8,
+            overwrite: "auto"
+          }),
+        onLeaveBack: () =>
+          gsap.to(".container", {
+            backgroundColor: prevColor,
+            duration: 0.8,
+            overwrite: "auto"
+          })
+      });
+    });
+  })
+
   return (
     <div>
       <Container>
-        <Left style={{ backgroundColor: "#E8E6DF" }}>
-          <Title>Not scrollable content</Title>
+
+        <Left className="container" ref={ref}>
+          <Section className="color-section" data-bg-color="#E8E6DF" >
+          </Section>
+          <Section className="color-section" data-bg-color="#CCE8F5">
+          </Section>
+          <Section className="color-section" data-bg-color="#DEDCC2">
+          </Section>
+          <Section className="color-section" data-bg-color="#FFC2D2">
+          </Section>
+          <Section className="color-section" data-bg-color="#BCC4A1">
+          </Section>
+          <Section className="color-section" data-bg-color="#355253">
+          </Section>
+          <Section className="color-section" data-bg-color="#E8E8E8">
+          </Section>
         </Left>
+
         <Right>
           <Image style={{ backgroundImage: "url(https://everland.dk/wp-content/uploads/2021/03/TuborgSquash_02-1440x1024.jpg)", backgroundSize: "cover" }}></Image>
           <Image style={{ backgroundImage: "url(https://everland.dk/wp-content/uploads/2021/04/Danone-Fjord-Brand-Design-Case-7-1-2400x1351.jpg)", backgroundSize: "cover" }}></Image>
@@ -49,9 +113,8 @@ export const Products = () => {
           <Image style={{ backgroundImage: "url(https://images.unsplash.com/photo-1609047286215-7dbe2ae99109?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2933&q=80)", backgroundSize: "cover" }}></Image>
           <Image style={{ backgroundImage: "url(https://everland.dk/wp-content/uploads/2021/03/Laesk_Can_06-2400x1350.jpg)", backgroundSize: "cover" }}></Image>
           <Image style={{ backgroundImage: "url(https://everland.dk/wp-content/uploads/2021/03/1Artboard10copy2-1800x1012.jpg)", backgroundSize: "cover" }}></Image>
-
-
         </Right>
+
       </Container>
     </div>
   )
